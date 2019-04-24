fd07f7667459d2150a4759c10c1e2193a3d8545b
santanarscs <raphaelstn@gmail.com>
2019-04-24 20:53:54 -0300

    config sentry

---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dsn: "https://17772021ffd541f6a2c7fb58445bb79a@sentry.io/1446071"
+};
--- src/server.js
+++ src/server.js
@@ -1,18 +1,23 @@
 const express = require("express");
+const Sentry = require("@sentry/node");
 const mongoose = require("mongoose");
 const databaseConfig = require("./config/database");
 const validate = require("express-validation");
 const Youch = require("youch");
+const sentryConfig = require("./config/sentry");
 class App {
   constructor() {
     this.express = express();
     this.isDev = process.env.NODE_ENV !== "production";
+    this.sentry();
     this.database();
     this.middlewares();
     this.routes();
     this.exception();
   }
-
+  sentry() {
+    Sentry.init(sentryConfig);
+  }
   database() {
     mongoose.connect(databaseConfig.uri, {
       useCreateIndex: true,
@@ -21,12 +26,17 @@ class App {
   }
   middlewares() {
     this.express.use(express.json());
+    this.express.use(Sentry.Handlers.requestHandler());
   }
 
   routes() {
     this.express.use(require("./routes"));
   }
   exception() {
+    if (process.env.NODE_ENV === "production") {
+      this.express.use(Sentry.Handlers.errorHandler());
+    }
+
     this.express.use(async (err, req, res, next) => {
       if (err instanceof validate.ValidationError) {
         return res.status(err.status).json(err);
